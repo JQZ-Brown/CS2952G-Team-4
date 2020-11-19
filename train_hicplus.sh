9018cd63826c0e18a22586ca3812ab50525eb1f6
Tyler DeFroscia <tdefrosc@node967.oscar.ccv.brown.edu>
2020-11-19 13:07:01 -0500

    hicplus training scripts + bugfixes

---
+++ train_hicplus.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#SBATCH -p gpu --gres=gpu:1
+#SBATCH -J HICPLUS
+#SBATCH -n 1
+#SBATCH -t 80:00:00
+#SBATCH --mem=24G
+
+hicplus train -i /users/tdefrosc/data/tdefrosc/coco_herbarium/GSE63525_K562_combined.hic -r 40 -c 19 -o model_epochnumber.model
