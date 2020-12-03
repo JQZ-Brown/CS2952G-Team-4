f64a501b1c1e50c67a566e58caad8f46891f3fe1
JQZ-Brown <72176815+JQZ-Brown@users.noreply.github.com>
2020-12-03 13:13:08 +0800

    Fix bugs of evaluation.

--- evaluation/evaluation.py
+++ evaluation/evaluation.py
@@ -87,8 +87,12 @@ def GenomeDISCO(mat1, mat2, t = 5):
         mat1_sub = _normalize(mat1[index])
         mat2_sub = _normalize(mat2[index])
         # Multiple t times
-        mat1_sub_t = multi_dot([mat1_sub for _ in range(t)])
-        mat2_sub_t = multi_dot([mat2_sub for _ in range(t)])
+        if t > 1:
+            mat1_sub_t = multi_dot([mat1_sub for _ in range(t)])
+            mat2_sub_t = multi_dot([mat2_sub for _ in range(t)])
+        else:
+            mat1_sub_t = mat1_sub
+            mat2_sub_t = mat2_sub
         # Compute GenomeDISCO
         mat1_non_zero = np.sum(np.sum(mat1_sub, axis = 1) > 0)
         mat2_non_zero = np.sum(np.sum(mat2_sub, axis=1) > 0)
@@ -121,15 +125,29 @@ def HiCRep(mat1, mat2):
         for i in range(0, shape[1]):
             for j in range(i, shape[1]):
                 index = K_index.index(j - i)
+                if mat1_sub[i, j] == 0 and mat2_sub[i, j] == 0:
+                    continue
                 mat1_K[index].append(mat1_sub[i, j])
                 mat2_K[index].append(mat2_sub[i, j])
                 Nk[index] += 1
         mat1_K = [np.array(each) for each in mat1_K]
         mat2_K = [np.array(each) for each in mat2_K]
-        Nk = np.array(Nk)
+        temp_mat1 = []
+        temp_mat2 = []
+        temp_Nk = []
+        for index in range(len(Nk)):
+            if (len(mat1_K[index]) != 0 and len(mat2_K[index]) != 0):
+                temp_mat1.append(mat1_K[index])
+                temp_mat2.append(mat2_K[index])
+                temp_Nk.append(Nk[index])
+        mat1_K = temp_mat1
+        mat2_K = temp_mat2
+        Nk = np.array(temp_Nk)
         # compute rho for every shard
-        r1 = [np.mean(mat1_K[k]*mat2_K[k])-np.mean(mat1_K[k])*np.mean(mat2_K[k]) for k in range(len(K_index))]
-        r2 = [np.sqrt(np.var(mat1_K[k]) * np.var(mat2_K[k])) for k in range(len(K_index))]
+        r1 = [np.nanmean(mat1_K[k]*mat2_K[k])-np.nanmean(mat1_K[k])*np.nanmean(mat2_K[k]) for k in range(len(mat1_K))]
+        r1 = [each if not np.isnan(each) else 0.0 for each in r1]
+        r2 = [np.sqrt(np.nanvar(mat1_K[k]) * np.nanvar(mat2_K[k])) for k in range(len(mat1_K))]
+        r2 = [each if not np.isnan(each) else 0.0 for each in r2]
         rho = np.array(r1) / np.array(r2)
         rho = np.nan_to_num(rho)
         weight = r2*Nk / np.sum(r2*Nk)
@@ -158,27 +176,27 @@ def MSE(mat1, mat2):
     return metric_record
 
 
-def SSIM(mat1, mat2):
-    raise NotImplementedError("Some problems with calculating the SSIM.")
-    mat1, mat2 = _preprocessing(mat1, mat2)
-    shape = mat1.shape
-    num_of_samples = shape[0]
-    metric_record = []
-    for index in range(num_of_samples):
-        mat1_sub = mat1[index]
-        mat2_sub = mat2[index]
-        MAX = np.max(mat1_sub) - np.min(mat1_sub)
-        C1 = (0.01 * MAX)**2
-        C2 = (0.01 * MAX) ** 2
-        mat1_mean = np.mean(mat1_sub)
-        mat2_mean = np.mean(mat2_sub)
-        mat1_sigma = np.std(mat1_sub)
-        mat2_sigma = np.std(mat2_sub)
-        sigma = np.cov(mat1_sub, mat2_sub)
-        # ssim = ((2*mat1_mean*mat2_mean + C1) * (2*sigma + C2)) / ((mat1_mean**2 + mat2_mean**2 + C1) * (mat1_sigma**2 + mat2_sigma**2 + C2))
-        ssim = structural_similarity(mat1_sub, mat2_sub, data_range= mat1_sub.max() - mat1_sub.min())
-        metric_record.append(ssim)
-    return metric_record
+# def SSIM(mat1, mat2):
+#     raise NotImplementedError("Some problems with calculating the SSIM.")
+#     mat1, mat2 = _preprocessing(mat1, mat2)
+#     shape = mat1.shape
+#     num_of_samples = shape[0]
+#     metric_record = []
+#     for index in range(num_of_samples):
+#         mat1_sub = mat1[index]
+#         mat2_sub = mat2[index]
+#         MAX = np.max(mat1_sub) - np.min(mat1_sub)
+#         C1 = (0.01 * MAX)**2
+#         C2 = (0.01 * MAX) ** 2
+#         mat1_mean = np.mean(mat1_sub)
+#         mat2_mean = np.mean(mat2_sub)
+#         mat1_sigma = np.std(mat1_sub)
+#         mat2_sigma = np.std(mat2_sub)
+#         sigma = np.cov(mat1_sub, mat2_sub)
+#         # ssim = ((2*mat1_mean*mat2_mean + C1) * (2*sigma + C2)) / ((mat1_mean**2 + mat2_mean**2 + C1) * (mat1_sigma**2 + mat2_sigma**2 + C2))
+#         ssim = structural_similarity(mat1_sub, mat2_sub, data_range= mat1_sub.max() - mat1_sub.min())
+#         metric_record.append(ssim)
+#     return metric_record
 
 
 def PSNR(mat1, mat2):
@@ -238,6 +256,32 @@ def PearsonCorre(mat1, mat2):
         # correlation = pearsonr(mat1_sub, mat2_sub)[0]
         metric_record.append(correlation)
     return np.array(metric_record)
+
+
+def PearsonCorreAll(mat1, mat2):
+    '''
+    Compute Pearson correlation measurement between two list of Hi-C matrices.
+    :param mat1: A list of matrices (num of samples, num of features, num of features).
+    :param mat2: Another list of matrices (num of samples, num of features, num of features).
+    :param r: Parameter of Spector (default = r).
+    :return: Spector measurements of all samples (num of samples, ).
+    '''
+    # For now, assume the distance shard is the same as bin size
+    mat1, mat2 = _preprocessing(mat1, mat2)
+    shape = mat1.shape
+    num_of_samples = shape[0]
+    metric_record = []
+    for index in range(num_of_samples):
+        mat1_sub = mat1[index]
+        mat2_sub = mat2[index]
+        # compute Pearson correlation for every shard
+        correlation = pearsonr(mat1_sub.reshape((shape[1]*shape[2],)), mat2_sub.reshape((shape[1]*shape[2],)))[0]
+        # mat1_sub = mat1[index].reshape((shape[1]*shape[2], ))
+        # mat2_sub = mat2[index].reshape((shape[1]*shape[2], ))
+        # correlation = pearsonr(mat1_sub, mat2_sub)[0]
+        metric_record.append(correlation)
+    return np.array(metric_record)
+
 
 # For test. Ignore this function
 def _extractData():
@@ -334,12 +378,12 @@ if __name__ == '__main__':
     print("Low res sub shape : ", lowres_sub.shape)
 
     spector = Spector(highres_sub, lowres_sub, r = 20)
-    genome_disco = GenomeDISCO(highres_sub, lowres_sub, t = 5)
+    genome_disco = GenomeDISCO(highres_sub, lowres_sub, t = 1)
     hic_rep = HiCRep(highres_sub, lowres_sub)
     mse = MSE(highres_sub, lowres_sub)
     psnr = PSNR(highres_sub, lowres_sub)
     pearson = PearsonCorre(highres_sub, lowres_sub)
-
+    person_all = PearsonCorreAll(highres_sub, lowres_sub)
     # Do not use SSIM for now.
     # ssim = SSIM(highres_sub, lowres_sub)
 
@@ -349,3 +393,4 @@ if __name__ == '__main__':
     print("MSE : ", mse)
     print("PSNR : ", psnr)
     print("Pearson Correlation : ", np.nanmean(pearson,axis=1))
+    print("Pearson Correlation All : ", person_all)
